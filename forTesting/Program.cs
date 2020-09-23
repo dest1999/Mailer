6c11cb252b9d9b86c7722f9c8b4f249d575b05e5
OlegSvrk <serv_all@mail.ru>
2020-09-23 19:18:28 +0300

    добавление проекта для тестирования

---
+++ forTesting/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace forTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
