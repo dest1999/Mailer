b38e6a3c6e3a89704be4d824dd47bda0a62b53c3
OlegSvrk <serv_all@mail.ru>
2020-09-24 22:57:06 +0300

    Ввод имен и пароля, ввод темы и тела письма, обработка исключения при отправке.

--- Mailer/MainWindow.xaml.cs
+++ Mailer/MainWindow.xaml.cs
@@ -12,7 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using System.Net;
+using System.Net.Mail;
 namespace Mailer
 {
     /// <summary>
@@ -20,9 +21,54 @@ namespace Mailer
     /// </summary>
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void SendMethod() { 
+            var mailSender = new MailAddress(tbSenderName.Text);
+            var mailRecipient = new MailAddress(tbRecipientName.Text);
+            bool isSendError = false;
+            using var message = new MailMessage(mailSender, mailRecipient)
+            {
+                Subject = tbSubject.Text,
+                Body = (tbMessage.Text == "") ? $"Письмо отправлено {DateTime.Now}" : $"{tbMessage.Text}\r\n\nПисьмо отправлено {DateTime.Now}"
+            };
+
+            using (var client = new SmtpClient("smtp.mail.ru", 25))
+            {
+                string login = mailSender.Address.ToString();
+
+                client.Credentials = new NetworkCredential(login, passwordBox.SecurePassword );
+                client.EnableSsl = (bool)chbUseSSL.IsChecked;
+
+                try
+                {
+                    client.Send(message);
+
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("oops, something wrong");
+                    isSendError = true;
+                }
+                finally
+                {
+                    if (!isSendError)
+                        MessageBox.Show("sended");
+                }
+
+            };
+
+        }
+
+        private void btnSend_Click(object sender, RoutedEventArgs e)
+        {
+            SendMethod();
+        }
+
+
     }
 }
