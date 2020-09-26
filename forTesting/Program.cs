42ba136d5ec94fc8c74931828df003b905efb44c
Oleg <serv_all@mail.ru>
2020-09-26 14:11:23 +0300

    forTesting определяет IP (например внешний адрес роутера) и шлёт на почту, работает полностью из консоли, бесконечный цикл с задержкой выполнения

--- forTesting/Program.cs
+++ forTesting/Program.cs
@@ -1,39 +1,65 @@
 ﻿using System;
-
+using System.Net.NetworkInformation;
+using System.Threading;
 using System.Net;
 using System.Net.Mail;
 namespace forTesting
 {
     class Program
     {
-        static void Main(string[] args)
+        static string PingTrace(string toHost, int ttl)
         {
-            var sender = new MailAddress("serv_all@mail.ru", "send");
-            var recipient = new MailAddress("serv_all@bk.ru");
+            byte[] arr = { 0x7B, 0x22, 0x62 };
+            var pinger = new Ping();
+            var pingReply = pinger.Send(toHost, 1000, arr, new PingOptions(ttl, true));
+            return pingReply.Address.ToString();
+        }
+
+        static void SendMessage(string mailUserName, string mailPassword, string body)
+        {
+            var sender = new MailAddress(mailUserName);
+            var recipient = new MailAddress(mailUserName);
 
             using var message = new MailMessage(sender, recipient)
             {
-                Subject = "Тема письма",
-                Body = $"Письмо отправлено {DateTime.Now}"
+                Subject = "New address",
+                Body = body
             };
 
             using (var client = new SmtpClient("smtp.mail.ru", 25))
             {
-                string login = "serv_all@mail.ru";
-                string password = "gggggggggggg"; //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-                client.Credentials = new NetworkCredential(login, password);
+                client.Credentials = new NetworkCredential(mailUserName, mailPassword);
                 client.EnableSsl = true;
-                
+
                 client.Send(message);
 
 
             };
+        }
 
+        static void Main(string[] args)//args: mailUserName, mailPassword, hostToPing, ttl
+        {
+            string mailUserName = args[0],
+                mailPassword = args[1],
+                hostToPing = args[2],
+                currentIP = "",
+                mbNewIP;
+            int ttl = Convert.ToInt32(args[3]);
 
+            while (true)
+            {
+                mbNewIP = PingTrace(hostToPing, ttl);
+                if (currentIP != mbNewIP)
+                {
+                    currentIP = mbNewIP;
+                    Console.WriteLine($"Your IP is {currentIP}, {DateTime.Now}");
+                    SendMessage(mailUserName, mailPassword, currentIP);
 
-            Console.WriteLine("Sending complete");
-            Console.ReadKey();
+                }
+                Thread.Sleep(60000);
+            }
+            
         }
     }
 }
