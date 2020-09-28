50195de617e9b8b141dec3534169668fa8ed4bff
OlegSvrk <serv_all@mail.ru>
2020-09-28 21:10:25 +0300

    Наполнение моделей

---
+++ Mailer/Models/Server.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mailer.Models
+{
+    class Server
+    {
+        string Address { get; set; }
+        int Port { get; set; }
+        string UseSSL { get; set; }
+        string Login { get; set; }
+        string Pssword { get; set; }
+        string Description { get; set; }
+    }
+}
