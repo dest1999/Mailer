47b4871dbe785dfe64fad5020a2c0e931d1e90ac
OlegSvrk <serv_all@mail.ru>
2020-10-02 14:51:14 +0300

    Lesson3... Пробуем...

---
+++ testWPF/ViewModels/BaseViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+
+namespace testWPF.ViewModel
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+}
---
+++ testWPF/ViewModels/MainWindowsViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using testWPF.ViewModel;
+
+namespace testWPF.ViewModels
+{
+    public class MainWindowsViewModel : BaseViewModel
+    {
+        private string _syncText;
+        public string SyncText
+        {
+            get => _syncText;
+            set
+            {
+                _syncText = value;
+                OnPropertyChanged(nameof(SyncText));
+            }
+        }
+
+    }
+}
